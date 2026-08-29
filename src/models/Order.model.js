71095f8f14f237e4d273cb08a286379124eb3fd2
osama-samy <osama@example.com>
2026-08-29 23:30:23 +0300

    create orderSchema

---
+++ src/models/Order.model.js
@@ -0,0 +1,158 @@
+import mongoose from "mongoose";
+
+// create orderSchema
+const orderSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+
+    items: [
+      {
+        product: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Product",
+          required: true,
+        },
+        name: {
+          type: String,
+          required: true,
+        },
+        image: {
+          type: String,
+          required: true,
+        },
+        price: {
+          type: Number,
+          required: true,
+          min: 0,
+        },
+        quantity: {
+          type: Number,
+          required: true,
+          min: 1,
+        },
+      },
+    ],
+
+    shippingAddress: {
+      fullName: {
+        type: String,
+        required: true,
+      },
+      phone: {
+        type: String,
+        required: true,
+      },
+      country: {
+        type: String,
+        required: true,
+      },
+      city: {
+        type: String,
+        required: true,
+      },
+      address: {
+        type: String,
+        required: true,
+      },
+      postalCode: {
+        type: String,
+        required: true,
+      },
+    },
+
+    paymentMethod: {
+      type: String,
+      required: true,
+      enum: ["cash", "paypal", "stripe", "paymob"],
+      default: "cash",
+    },
+
+    paymentStatus: {
+      type: String,
+      required: true,
+      enum: ["pending", "paid", "failed", "refunded"],
+      default: "pending",
+    },
+
+    transactionId: {
+      type: String,
+      default: null,
+    },
+
+    subtotal: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+
+    shippingFee: {
+      type: Number,
+      default: 0,
+    },
+
+    tax: {
+      type: Number,
+      default: 0,
+    },
+
+    discount: {
+      type: Number,
+      default: 0,
+    },
+
+    totalPrice: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+
+    status: {
+      type: String,
+      required: true,
+      enum: [
+        "pending",
+        "confirmed",
+        "processing",
+        "shipped",
+        "delivered",
+        "cancelled",
+        "returned",
+      ],
+      default: "pending",
+    },
+
+    paidAt: {
+      type: Date,
+      default: null,
+    },
+
+    deliveredAt: {
+      type: Date,
+      default: null,
+    },
+
+    cancelledAt: {
+      type: Date,
+      default: null,
+    },
+
+    customerNote: {
+      type: String,
+      maxlength: 1000,
+    },
+
+    adminNote: {
+      type: String,
+      maxlength: 1000,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export default mongoose.model("Order", orderSchema);

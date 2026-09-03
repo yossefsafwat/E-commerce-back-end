6f42226e8c2082d23621ccf8b4c6d975fcb7bf95
osama-samy <osama@example.com>
2026-09-04 01:40:50 +0300

    create wishlistSchema

---
+++ src/models/Wishlist.model.js
@@ -0,0 +1,30 @@
+import mongoose from 'mongoose'
+
+const wishlistSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+      unique: true,
+    },
+    products: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Product',
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+)
+
+wishlistSchema.pre(/^find/, function (next) {
+  this.populate('products')
+  next()
+})
+
+const Wishlist = mongoose.model('Wishlist', wishlistSchema)
+
+export default Wishlist
